4e32c40157127d83443c9d357db6a830edcb2133
Yang <crea9813@gmail.com>
2019-11-29 01:37:43 +0900

    구글 소셜 로그인 로직 구현

--- SwiftUI_SocialLogin/ContentView.swift
+++ SwiftUI_SocialLogin/ContentView.swift
@@ -7,10 +7,22 @@
 //
 
 import SwiftUI
+import Firebase
+import GoogleSignIn
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        HStack{
+            VStack{
+                Button(action: SocialLogin.attemptLoginGoogle,label: {
+                        Image("google").frame(width: 20, height: 20)
+                })
+                    .padding(EdgeInsets(top: 8, leading: 16, bottom: 8, trailing: 16))
+                    .background(Color.white)
+                    .cornerRadius(8.0)
+                    .shadow(radius: 4.0)
+            }
+        }
     }
 }
 
@@ -18,4 +30,19 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct SocialLogin: UIViewRepresentable {
+
+    func makeUIView(context: UIViewRepresentableContext<SocialLogin>) -> UIView {
+        return UIView()
+    }
+
+    func updateUIView(_ uiView: UIView, context: UIViewRepresentableContext<SocialLogin>) {
+    }
+
+    static func attemptLoginGoogle(){
+        GIDSignIn.sharedInstance()?.presentingViewController = UIApplication.shared.windows.last?.rootViewController
+        GIDSignIn.sharedInstance()?.signIn()
+        }
 }
